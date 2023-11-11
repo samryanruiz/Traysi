66ee321b71de221f662c1d9d5c3539ec84d68725
samryanruiz <samryanruiz@gmail.com>
2023-11-11 14:47:44 +0800

    Add original files

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+
+import SplashScreen from "./screens/SplashScreen";
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator initialRouteName="Splash">
+            <Stack.Screen 
+              name="Splash" 
+              component={SplashScreen} 
+              options={{ headerShown: false }} 
+            />
+            <Stack.Screen 
+              name="Home" 
+              component={HomeScreen} 
+              options={{ headerShown: false }} 
+            />
+            <Stack.Screen 
+              name="MapScreen" 
+              component={MapScreen} 
+              options={{ headerShown: false }} 
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
+  );
+}
